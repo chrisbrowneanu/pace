f9a892b2e3dd6584ed3a580ba5fb85c3c8f44347
Chris Browne <chris.browne@anu.edu.au>
2020-02-01 16:49:25 +1100

    init commit

---
+++ pace.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+# python ./sCraPe.py
+#
+#
+# chris.browne@anu.edu.au - all care and no responsibility :)
+# ===========================================================
+
+"""grabs course descriptions on P&C and converts them into a document"""
+
+import urllib.request, urllib.parse
+import sys
+import os
+from pathlib import Path
+import yaml
+import fileinput
+import re
+import subprocess
+import shutil
+import pickle
+
+input_file = sys.argv[1]
+BASE_URL = sys.argv[2]
+
+
+def process_regex(in_file):
+    print_log("Cleaning up text files..", "info")
+    this_regex = cfg['regex']
+    for key, val in this_regex.items():
+        with fileinput.FileInput(in_file, inplace=True) as file:
+            for line in file:
+                this_sub = re.sub(r"" + key + "", r"" + val + "", line.rstrip())
+                print(this_sub)
+
+def load_yml(file):
+    '''Read in a YAML file'''
+    with open(file, 'r') as stream:
+        c = yaml.safe_load(stream)
+    return c
+
+def print_log(message, css):
+    with open(directory + "/tmp/" + "session_log.txt", "a") as out:
+        out.write("<li class=\"text-" + css + "\">" + message + "</li>")
+    print(message)
+
+def pandoc_html():
+    print_log("Converting to output format..", "info")
+    try:
+        print_log("Creating output file", "success")
+        subprocess.call("pandoc -s " + directory + "/out.html -o" + directory + "/out.docx", shell=True)
+    except Exception:
+        print("Unable to create feedback file", "danger")
+
+def mkfile(file):
+    f = open(file, "w+")
+    f.close
+
+def remove_newlines(file):
+    print_log("Processing text files..", "info")
+    clean = open(file).read().replace('\n', '')
+    with open(file, 'w') as file_write:
+        file_write.write(clean)
+        file_write.close
+
+def copy_sub_text(file_in):
+    with open(out, "a") as txt_write:
+        with open(file_in, "r") as html_read:
+            toggle_out = False
+            for line in html_read:
+                rule_start = [re.match(r".*secondary-navigation-wrapper", line)]
+                rule_end = [re.match(r".*<!-- END SUB PLANS -->", line)]
+                if any(rule_start):
+                    toggle_out = True
+                if any(rule_end):
+                    toggle_out = False
+                if toggle_out:
+                    txt_write.write(line)
+    txt_write.close
+
+def html_write(file, content):
+    with open(file, "wb") as html_write:
+        html_write.write(content)
+        html_write.close
+
+def html_retrieve(file):
+    print_log("Collecting HTML pages..", "info")
+    codes = open(file)
+    for code in codes:
+        this_code = code.rstrip()
+        this_html = directory + "/html/" + this_code + ".html"
+        html_exists = os.path.isfile(this_html)
+        if not html_exists:
+            print_log(this_code + "  ", "white")
+            url = BASE_URL + this_code
+            response = urllib.request.urlopen(url)
+            content = response.read()
+            html_write(this_html, content)
+        copy_sub_text(this_html)
+
+def this_pickle(count):
+    settings = {"exit": count}
+    print(directory)
+    pickle.dump(settings, open(directory + "/tmp/settings.txt", "wb"))
+
+def file_directory(file):
+    """set up input file information"""
+    this_file = Path(file)
+    global basename, directory, extension
+    basename = str(this_file.stem)
+    directory = str(this_file.parent)
+    extension = str(this_file.suffix)
+
+def setup_files():
+    # shutil.rmtree('/job/', ignore_errors=True)
+    # shutil.rmtree('/download/', ignore_errors=True)
+    # os.makedirs('/job/html/')
+    # os.makedirs('/job/txt/')
+    # os.makedirs('/job/tmp/')
+    global cfg, out, session_log
+    cfg = load_yml("./includes/yml/replace.yml")
+    out = directory + "/out.html"
+    session_log = directory + "/tmp/session_log.txt"
+    mkfile(out)
+    mkfile(session_log)
+
+def check_codes(file):
+    fread = open(input_file)
+    global course_codes
+    course_codes=[]
+    for line in fread:
+        line = line.rstrip()
+        # rules = [re.match(r"^..../d", line)]
+        # if any(rules):
+        course_codes.append(line.rstrip())
+    count_course_codes = len(course_codes)
+    print(count_course_codes)
+    if count_course_codes == 0:
+        print_log("Found " + str(count_course_codes) + " courses. Please try another file.", "danger")
+    else:
+        print_log("Found " + str(count_course_codes) + " courses", "success")
+
+
+def main():
+    file_directory(input_file)
+    this_pickle(-1)
+    setup_files()
+    print_log("Using " + BASE_URL, "success")
+    check_codes(input_file)
+    html_retrieve(input_file)
+    remove_newlines(out)
+    process_regex(out)
+    pandoc_html()
+    this_pickle(len(course_codes))
+    print_log("Process complete! Ready to Download", "success")
+
+if __name__ == "__main__":
+    main()
---
+++ web_app.py
@@ -0,0 +1,180 @@
+from flask_bootstrap import Bootstrap
+from flask import request, send_file, redirect, render_template, Flask
+import subprocess, os, io, shutil, pathlib, zipfile, yaml, pickle
+from datetime import datetime
+from waitress import serve
+from webui import WebUI
+
+APP_DIR = '/Users/cb/Sites/pace'
+JOB_DIR = APP_DIR + '/job'
+DOWNLOAD_DIR = APP_DIR + '/download'
+HTML_DIR = JOB_DIR + '/html'
+TXT_DIR = JOB_DIR + '/txt'
+TMP_DIR = JOB_DIR + '/tmp'
+
+ALLOWED_EXTENSIONS = set(['txt'])
+
+global config
+
+app = Flask(__name__)
+# app = WebUI(app, debug=True)
+
+app.config["DEBUG"] = False
+
+app.config['JOB_DIR'] = JOB_DIR
+app.config['DOWNLOAD_DIR'] = DOWNLOAD_DIR
+app.config['HTML_DIR'] = HTML_DIR
+app.config['TXT_DIR'] = TXT_DIR
+app.config['TMP_DIR'] = TMP_DIR
+
+app.secret_key = "TvF8WJlOIdaAUkeeCxjsJem3X3gHRN8T"
+bootstrap = Bootstrap(app)
+
+# ===========
+# FUNCTIONS
+# ===========
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+def reset_files():
+    shutil.rmtree(app.config['JOB_DIR'], ignore_errors=True)
+    shutil.rmtree(app.config['DOWNLOAD_DIR'], ignore_errors=True)
+    os.makedirs(app.config['JOB_DIR'])
+    os.makedirs(app.config['DOWNLOAD_DIR'])
+    os.makedirs(app.config['HTML_DIR'])
+    os.makedirs(app.config['TXT_DIR'])
+    os.makedirs(app.config['TMP_DIR'])
+    with open(app.config['TMP_DIR'] + "/" + "session_log.txt", "w") as out:
+        out.write(cfg['msg']['session_log_default'])
+    with open(app.config['TMP_DIR'] + "/" + "download_msg.txt", "w") as out:
+        out.write(cfg['msg']['download_msg_default'])
+
+def load_config(file):
+    with open(file, 'r') as stream:
+        config_defaults = yaml.safe_load(stream)
+    return config_defaults
+
+def messaging():
+    settings = pickle.load(open(app.config['TMP_DIR'] + '/settings.txt', "rb"))
+    print(settings['exit'])
+    if settings['exit'] > 0:
+        with open(app.config['TMP_DIR'] + "/" + "session_log.txt", "a") as out:
+            out.write(cfg['msg']['session_log_complete'])
+        with open(app.config['TMP_DIR'] + "/" + "download_msg.txt", "w") as out:
+            out.write(cfg['msg']['download_msg_complete'])
+            out.write(cfg['msg']['download_msg_url'] + this_name + ".zip" + cfg['msg']['download_msg_url_archive'])
+    elif settings['exit'] == 0:
+        with open(app.config['TMP_DIR'] + "/" + "session_log.txt", "a") as out:
+            out.write(cfg['msg']['session_log_error'])
+        with open(app.config['TMP_DIR'] + "/" + "download_msg.txt", "w") as out:
+            out.write(cfg['msg']['download_msg_error'])
+            out.write(cfg['msg']['start_again_button'])
+    else:
+        with open(app.config['TMP_DIR'] + "/" + "session_log.txt", "a") as out:
+            out.write(cfg['msg']['session_log_fatal'])
+        with open(app.config['TMP_DIR'] + "/" + "download_msg.txt", "w") as out:
+            out.write(cfg['msg']['download_msg_fatal'])
+            out.write(cfg['msg']['start_again_button'])
+
+def upload():
+    file = request.files['file']
+    url = request.form['base_url']
+    global base_url
+    if url == "":
+        base_url = "https://programsandcourses.anu.edu.au/course/"
+    else:
+        base_url = url
+
+    if file:
+        if allowed_file(file.filename):
+            file_dir = os.path.join(app.config['JOB_DIR'], 'codes.txt')
+            file.save(file_dir)
+            return ('', 205)
+        else:
+            return render_template('error.html')
+
+def cleaner():
+    global this_name
+    now = datetime.now()
+    this_name = now.strftime("%Y%m%d%H%M%S")
+    shutil.make_archive(app.config['DOWNLOAD_DIR'] + "/" + this_name, 'zip', app.config['JOB_DIR'])
+    shutil.rmtree(app.config['HTML_DIR'], ignore_errors=True)
+    shutil.rmtree(app.config['TXT_DIR'], ignore_errors=True)
+
+def processing():
+    this_file = os.path.join(app.config['JOB_DIR'], 'codes.txt')
+    print(this_file)
+    print(base_url)
+    subprocess.call("python3 pace.py " + this_file + " " + base_url, shell=True)
+
+
+# ===========
+# VIEWS
+# ===========
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    reset_files()
+    if request.method == 'POST':
+        upload()
+        processing()
+        cleaner()
+        messaging()
+        return ('', 205)
+    return render_template('index.html')
+
+
+@app.route('/console_msg')
+def console_msg():
+    def generate():
+        with open(app.config['TMP_DIR'] + '/session_log.txt') as f:
+            while True:
+                content = f.read()
+                return content
+    return app.response_class(generate(), mimetype='text/plain')
+
+@app.route('/download_msg')
+def download_msg():
+    def generate_msg():
+        with open(app.config['TMP_DIR'] + '/download_msg.txt') as f:
+            while True:
+                content = f.read()
+                return content
+    return app.response_class(generate_msg(), mimetype='text/plain')
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
+
+@app.route('/download_file/<filename>')
+def download_remove_file(filename):
+    file_path = os.path.join(app.config['DOWNLOAD_DIR'], filename)
+    return_data = io.BytesIO()
+    with open(file_path, 'rb') as file_out:
+        return_data.write(file_out.read())
+    return_data.seek(0)
+    os.remove(file_path)
+    return send_file(
+        return_data,
+        mimetype='application/pdf',
+        as_attachment=True,
+        attachment_filename=filename)
+
+@app.route('/download_archive/<filename>')
+def download_remove_archive(filename):
+    path = os.path.join(app.config['DOWNLOAD_DIR'], filename)
+    def generate():
+        with open(path) as f:
+            yield from f
+        os.remove(path)
+    r = app.response_class(generate(), mimetype='application/zip')
+    r.headers.set('Content-Disposition', 'attachment', filename=filename)
+    return r
+
+
+cfg = load_config("./includes/yml/messages.yml")
+
+if __name__ == '__main__':
+    app.run()
